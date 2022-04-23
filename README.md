d8dedd3f686c1cef1216cb8493d2726186d10659
Luca Schultz <11065276-locxter@users.noreply.gitlab.com>
2022-04-23 14:42:56 +0200

    Uploaded already existing files.

---
+++ README.md
@@ -0,0 +1,33 @@
+# xrcsbkgmpd
+
+## Overview
+
+
+![Overview image](overview.png)
+
+This is an exercise bike gamepad I designed. In the default config it is set up to play [SuperTuxKart](https://supertuxkart.net/Main_Page) on a SportPlus SP-SRP-3000 exercise bike.
+
+## Dependencies
+
+I generally try to minimize dependencies, but I'm a one man crew and can therefore only support Debian as I'm running it myself. Anyway, you need to have the following packages installed for everything to work properly:
+
+- FreeCAD for editing and exporting the CAD files. Install it with `sudo apt install freecad`.
+- A2Plus for assemblying the different components in FreeCAD. Install it via the integrated addon manager.
+- Arduino IDE as a way to compile the Arduino code. Install it with `sudo apt install arduino`.
+
+## How to build it
+
+This project is relatively straight forward as it mainly consists of an Arduino Pro Micro, one reed switch and 2 capacitive touch buttons all housed in 3D printed enclosures or simply glued onto the exercise bike in the case of the reed switch. Take a look at the overview image at the top and assembly image down below for inspiration.
+
+![Assembly image](cad/assembly.png)
+
+Below is a full bill of materials with German sources for all non-printable parts:
+
+| Quantity: | Item: | Source: |
+| --- | --- | --- |
+| 1 | Instant glue | [Amazon](https://www.amazon.de/Pattex-Sekundenkleber-L%C3%B6sungsmittelfreier-Gel-Kleber-Transparent/dp/B00O23B8IE) |
+| 1 | Cables | [Amazon](https://www.amazon.de/Donau-Elektronik-GMBH-Original-Kupfer/dp/B01BI1G88C) |
+| 1 | Perfboard, pin header and screw terminal kit | [Amazon](https://www.amazon.de/70Stk-Doppelseitig-Lochrasterplatte-Kit-Lochrasterplatine/dp/B07BDKG68Q) |
+| 1 | Arduino Pro Micro | [Amazon](https://www.amazon.de/Micro-ATmega32U4-Arduino-Leonardo-%C3%A4hnlich/dp/B01D0OI90U) |
+| 1 | Reed switch | [Amazon](https://www.amazon.de/Gebildet-Magnetschalter-Magnetischer-Normalerweise-Geschlossener/dp/B085XQLQ3N) |
+| 2 | Capacitive touch sensors | [Amazon](https://www.amazon.de/YOUMILE-Touch-Switch-Taste-Selbstsperrmodul-30-Pin-Header-Dupont-Leitung/dp/B07TTH5ZH9) |
---
+++ xrcsbkgmpd.ino
@@ -0,0 +1,106 @@
+#include <Keyboard.h>
+
+// Pins
+const int LEFT_TRIGGER_PIN = 2;
+const int RIGHT_TRIGGER_PIN = 3;
+const int ROTATION_SENSOR_PIN = 4;
+
+// Other variables
+unsigned long lastRotation = 0;
+float rpm = 0, avgOldRpm = 0, oldRpms[30];
+bool isUpPressed = false, isLeftPressed = false, isRightPressed = false, isPaused = false;
+
+// Setup function
+void setup() {
+    // Define pins as inputs
+    pinMode(ROTATION_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(LEFT_TRIGGER_PIN, INPUT);
+    pinMode(RIGHT_TRIGGER_PIN, INPUT);
+    // Initialize keyboard functionality
+    Keyboard.begin();
+    // Wait for activation by user
+    while (digitalRead(LEFT_TRIGGER_PIN) == LOW || digitalRead(RIGHT_TRIGGER_PIN) == LOW) {
+        delay(100);
+    }
+    lastRotation = millis();
+}
+
+// Main function
+void loop() {
+    // Calculate current RPM on every rotation when the user is pedalling and respond with a game input
+    if (digitalRead(ROTATION_SENSOR_PIN) == HIGH && millis() - lastRotation > 200 && millis() - lastRotation < 2000) {
+        // Resume the game if it was previously paused
+        if (isPaused) {
+            Keyboard.write(KEY_ESC);
+            delay(100);
+            isPaused = false;
+        }
+        // Calculate RPM and average old RPM
+        rpm = 60000.0 / (millis() - lastRotation);
+        avgOldRpm = (oldRpms[0] + oldRpms[1] + oldRpms[2] + oldRpms[3] + oldRpms[4] + oldRpms[5] + oldRpms[6] + oldRpms[7] + oldRpms[8] + oldRpms[9] + oldRpms[10] + oldRpms[11] + oldRpms[12] + oldRpms[13] + oldRpms[14] + oldRpms[15] + oldRpms[16] + oldRpms[17] + oldRpms[18] + oldRpms[19] + oldRpms[20] + oldRpms[21] + oldRpms[22] + oldRpms[23] + oldRpms[24] + oldRpms[25] + oldRpms[26] + oldRpms[27] + oldRpms[28] + oldRpms[29]) / 30.0;
+        // Accelerate + Nitro
+        if (rpm > avgOldRpm + 5) {
+            if (!isUpPressed) {
+                Keyboard.press(KEY_UP_ARROW);
+                isUpPressed = true;
+            }
+            Keyboard.write('N');
+        }
+        // Brake
+        else if (rpm < avgOldRpm - 5 && isUpPressed) {
+            Keyboard.release(KEY_UP_ARROW);
+            isUpPressed = false;
+        }
+        // Accelerate
+        else if (rpm <= avgOldRpm + 5 && rpm >= avgOldRpm - 5 && !isUpPressed) {
+            Keyboard.press(KEY_UP_ARROW);
+            isUpPressed = true;
+        }
+        // Update old RPMs
+        for (int i = 29; i > 0; i--) {
+            oldRpms[i] = oldRpms[i - 1];
+        }
+        oldRpms[0] = rpm;
+        lastRotation = millis();
+    }
+    // Pause game when user stops pedalling
+    if (millis() - lastRotation > 2000) {
+        if (!isPaused) {
+            Keyboard.releaseAll();
+            Keyboard.write(KEY_ESC);
+            rpm = 0;
+            avgOldRpm = 0;
+            for (int i = 0; i < 30; i++) {
+                oldRpms[i] = 0;
+            }
+            isUpPressed = false;
+            isLeftPressed = false;
+            isRightPressed = false;
+            isPaused = true;
+        }
+        lastRotation = millis();
+    }
+    if (!isPaused) {
+        // Fire
+        if (digitalRead(LEFT_TRIGGER_PIN) == HIGH && digitalRead(RIGHT_TRIGGER_PIN) == HIGH) {
+            Keyboard.write(' ');
+        } else {
+            // Steer left
+            if (digitalRead(LEFT_TRIGGER_PIN) == HIGH && !isLeftPressed) {
+                Keyboard.press(KEY_LEFT_ARROW);
+                isLeftPressed = true;
+            } else if (digitalRead(LEFT_TRIGGER_PIN) == LOW && isLeftPressed) {
+                Keyboard.release(KEY_LEFT_ARROW);
+                isLeftPressed = false;
+            }
+            // Steer right
+            if (digitalRead(RIGHT_TRIGGER_PIN) == HIGH && !isRightPressed) {
+                Keyboard.press(KEY_RIGHT_ARROW);
+                isRightPressed = true;
+            } else if (digitalRead(RIGHT_TRIGGER_PIN) == LOW && isRightPressed) {
+                Keyboard.release(KEY_RIGHT_ARROW);
+                isRightPressed = false;
+            }
+        }
+    }
+}
