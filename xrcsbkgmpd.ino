d3c5400ae0b7ae805abd9ae9a9870c6939d1f373
locxter <54595101+locxter@users.noreply.github.com>
2023-04-07 19:46:58 +0200

    Reformatted the code.

--- xrcsbkgmpd.ino
+++ xrcsbkgmpd.ino
@@ -35,7 +35,8 @@ void setup() {
 void loop() {
     currentMillis = millis();
     // Calculate current RPM on every rotation when the user is pedalling and respond with a game input
-    if (digitalRead(ROTATION_SENSOR_PIN) == HIGH && currentMillis - lastRotation >= 200 && currentMillis - lastRotation <= 2000) {
+    if (digitalRead(ROTATION_SENSOR_PIN) == HIGH && currentMillis - lastRotation >= 200 &&
+        currentMillis - lastRotation <= 2000) {
         // Resume the game if it was previously paused
         if (isPaused) {
             Keyboard.write(KEY_ESC);
@@ -44,7 +45,12 @@ void loop() {
         }
         // Calculate RPM and average old RPM
         rpm = 60000.0 / (currentMillis - lastRotation);
-        avgOldRpm = (oldRpms[0] + oldRpms[1] + oldRpms[2] + oldRpms[3] + oldRpms[4] + oldRpms[5] + oldRpms[6] + oldRpms[7] + oldRpms[8] + oldRpms[9] + oldRpms[10] + oldRpms[11] + oldRpms[12] + oldRpms[13] + oldRpms[14] + oldRpms[15] + oldRpms[16] + oldRpms[17] + oldRpms[18] + oldRpms[19] + oldRpms[20] + oldRpms[21] + oldRpms[22] + oldRpms[23] + oldRpms[24] + oldRpms[25] + oldRpms[26] + oldRpms[27] + oldRpms[28] + oldRpms[29]) / 30.0;
+        avgOldRpm = (oldRpms[0] + oldRpms[1] + oldRpms[2] + oldRpms[3] + oldRpms[4] + oldRpms[5] + oldRpms[6] +
+                     oldRpms[7] + oldRpms[8] + oldRpms[9] + oldRpms[10] + oldRpms[11] + oldRpms[12] + oldRpms[13] +
+                     oldRpms[14] + oldRpms[15] + oldRpms[16] + oldRpms[17] + oldRpms[18] + oldRpms[19] + oldRpms[20] +
+                     oldRpms[21] + oldRpms[22] + oldRpms[23] + oldRpms[24] + oldRpms[25] + oldRpms[26] + oldRpms[27] +
+                     oldRpms[28] + oldRpms[29]) /
+                    30.0;
         // Accelerate + Nitro
         if (rpm > avgOldRpm + 5) {
             if (!isUpPressed) {
